61c59e26cb3147dfdc6aa665c5c8bcee0305ca9e
sunyang <sunyang@wondersgroup.com>
2019-05-31 16:06:37 +0800

    居住地址拼接方法

--- src/main/java/com/wonders/health/tumor/common/service/AreaService.java
+++ src/main/java/com/wonders/health/tumor/common/service/AreaService.java
@@ -20,5 +20,32 @@ public class AreaService {
         return cancerDicAreaDao.findAreas(level, pcode);
     }
 
+    public String getFullAddress(String province, String city, String county, String town, String committee, String detail) {
+        CancerDicArea provinceArea = cancerDicAreaDao.get(province);
+        CancerDicArea cityArea = cancerDicAreaDao.get(city);
+        CancerDicArea countyArea = cancerDicAreaDao.get(county);
+        CancerDicArea townArea = cancerDicAreaDao.get(town);
+        CancerDicArea committeeArea = cancerDicAreaDao.get(committee);
+        StringBuffer sb = new StringBuffer();
+
+        if (provinceArea != null) {
+            sb.append(provinceArea.getCname());
+        }
+        if (cityArea != null) {
+            sb.append(cityArea.getCname());
+        }
+        if (countyArea != null) {
+            sb.append(countyArea.getCname());
+        }
+        if (townArea != null) {
+            sb.append(townArea.getCname());
+        }
+        if (committeeArea != null) {
+            sb.append(committeeArea.getCname());
+        }
+        sb.append(detail);
+        return sb.toString();
+    }
+
 
 }
